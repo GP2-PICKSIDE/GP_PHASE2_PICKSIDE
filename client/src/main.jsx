1a39383864662eb8552de98b8d766bf72e8d51f3
gabrielavania <gabriela.vania98@gmail.com>
2025-08-27 13:49:11 +0700

    feat: added question Ai on front end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,17 +12,17 @@ import QuizRoomPage from "./views/QuizRoomPage";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/room" element={<RoomPage />} />
-            <Route path="/quiz-room" element={<QuizRoomPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  </StrictMode>
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/room" element={<RoomPage />} />
+          <Route path="/quiz-room" element={<QuizRoomPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
+  // </StrictMode>
 );
