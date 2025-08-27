bd0a8f9681abeb953f351cd048dc1a55063eb98e
raxelf <frananda.rafael@gmail.com>
2025-08-27 14:42:18 +0700

    fix: socket on join

--- client/src/stores/socketStore.js
+++ client/src/stores/socketStore.js
@@ -19,6 +19,7 @@ const useSocketStore = create(
         useGameStore.setState((state) => ({
           code: room.code,
           gameState: room.gameState,
+          roomName: room.roomName,
           players: room.players,
           settings: room.settings,
           roundIndex: room.roundIndex,
@@ -30,8 +31,6 @@ const useSocketStore = create(
             : state.isHost,
           me: { ...state.me, id: socketInitializer.id },
         }));
-
-        console.log(`Room with code ${useGameStore.getState().code} created`);
       }),
         set((state) => {
           state.socketState = socketInitializer;
