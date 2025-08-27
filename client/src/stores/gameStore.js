0f4cc1eeff70ccf2663642f52d5e2820a0af380d
raxelf <frananda.rafael@gmail.com>
2025-08-27 13:19:09 +0700

    add in round, reveal chart, summary page

--- client/src/stores/gameStore.js
+++ client/src/stores/gameStore.js
@@ -13,7 +13,7 @@ const useGameStore = create(
 
     settings: { theme: "funny", lang: "id", rounds: 5 },
 
-    gameState: "summary",
+    gameState: "idle",
     players: [],
     roundIndex: 0,
     totalRounds: 0,
