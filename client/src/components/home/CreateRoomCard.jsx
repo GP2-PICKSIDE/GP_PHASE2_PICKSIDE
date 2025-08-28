1a4d9d4105933391b114b76f7e64966178b48ae5
gabrielavania <gabriela.vania98@gmail.com>
2025-08-28 14:00:50 +0700

    fix: bug

--- client/src/components/home/CreateRoomCard.jsx
+++ client/src/components/home/CreateRoomCard.jsx
@@ -45,8 +45,7 @@ const CreateRoomcard = () => {
           <div className="flex flex-col gap-2">
             <label
               htmlFor="displayName"
-              className="text-sm text-gray-700 font-medium"
-            >
+              className="text-sm text-gray-700 font-medium">
               {lang === "en"
                 ? en.createRoom.displayName
                 : id.createRoom.displayName}
@@ -58,8 +57,7 @@ const CreateRoomcard = () => {
                 className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-900"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                fill="currentColor"
-              >
+                fill="currentColor">
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H18C18 18.6863 15.3137 16 12 16C8.68629 16 6 18.6863 6 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11Z"></path>
               </svg>
@@ -82,8 +80,7 @@ const CreateRoomcard = () => {
           <div className="flex flex-col gap-2">
             <label
               htmlFor="roomName"
-              className="text-sm text-gray-700 font-medium"
-            >
+              className="text-sm text-gray-700 font-medium">
               {lang === "en" ? en.createRoom.roomName : id.createRoom.roomName}
               <span className="text-red-500">*</span>
             </label>
@@ -92,8 +89,7 @@ const CreateRoomcard = () => {
                 className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-900"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                fill="currentColor"
-              >
+                fill="currentColor">
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path d="M19 21H5C4.44772 21 4 20.5523 4 20V11L1 11L11.3273 1.6115C11.7087 1.26475 12.2913 1.26475 12.6727 1.6115L23 11L20 11V20C20 20.5523 19.5523 21 19 21ZM6 19H18V9.15745L12 3.7029L6 9.15745V19Z"></path>
               </svg>
@@ -114,9 +110,8 @@ const CreateRoomcard = () => {
 
           <button
             type="submit"
-            className={`w-full rounded-lg px-6 py-3 font-semibold text-white transition transform cursor-pointer bg-primary`}
-          >
-            {lang === "en" ? en.createRoombutton : id.createRoom.button}
+            className={`w-full rounded-lg px-6 py-3 font-semibold text-white transition transform cursor-pointer bg-primary`}>
+            {lang === "en" ? en.createRoom.button : id.createRoom.button}
           </button>
 
           <p className="text-center text-gray-500 text-sm">
--- client/src/components/room/summary/Summary.jsx
+++ client/src/components/room/summary/Summary.jsx
@@ -143,8 +143,9 @@ const Summary = () => {
                         }%)`
                     : lang === "en"
                     ? `${en.summary.overallLeaderA} (+${overallB - overallA}%)`
-                    : `${id.summary.overallLeaderA}`}{" "}
-                  {`(+${overallB - overallA}%)`}
+                    : `${id.summary.overallLeaderA} (+${
+                        overallB - overallA
+                      }%)`}{" "}
                 </p>
               )}
             </div>
@@ -175,14 +176,12 @@ const Summary = () => {
           {/* CTAs */}
           <button
             onClick={handlePlayAgain}
-            className="w-full bg-indigo-600 hover:bg-indigo-700 p-3 rounded-xl text-white font-semibold cursor-pointer transition"
-          >
+            className="w-full bg-indigo-600 hover:bg-indigo-700 p-3 rounded-xl text-white font-semibold cursor-pointer transition">
             {lang === "en" ? en.summary.playAgain : id.summary.playAgain}
           </button>
           <Link
             to="/play"
-            className="w-full border border-gray-300 p-3 rounded-xl cursor-pointer hover:bg-gray-100 text-center"
-          >
+            className="w-full border border-gray-300 p-3 rounded-xl cursor-pointer hover:bg-gray-100 text-center">
             {lang === "en" ? en.summary.backHome : id.summary.backHome}
           </Link>
         </div>
@@ -208,8 +207,7 @@ const Summary = () => {
               return (
                 <article
                   key={i}
-                  className="rounded-xl p-6 shadow-sm border border-black/5 bg-white"
-                >
+                  className="rounded-xl p-6 shadow-sm border border-black/5 bg-white">
                   <h3 className="font-semibold text-gray-900">
                     {i + 1}. {round?.question?.question}
                   </h3>
