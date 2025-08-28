2699a8130ddfcbf27f1aa6c8d59a00b1096631a0
raxelf <frananda.rafael@gmail.com>
2025-08-28 10:38:41 +0700

    feat: add i18n for en & id

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./index.css";
 
 import HomePage from "./views/HomePage";
@@ -9,21 +8,17 @@ import RootLayout from "./layouts/RootLayout";
 import RoomPage from "./views/RoomPage";
 import MainPage from "./views/MainLandingPage";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
 
-        <Route element={<RootLayout />}>
-          <Route path="/play" element={<HomePage />} />
-          <Route path="/room" element={<RoomPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>
+      <Route element={<RootLayout />}>
+        <Route path="/play" element={<HomePage />} />
+        <Route path="/room" element={<RoomPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   // </StrictMode>
 );
